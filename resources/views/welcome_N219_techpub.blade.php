ab28946b2af23e99980ae08356e7a651d0bf640d
Ferdi Saptoko <ferdisaptoko@gmail.com>
2023-07-18 16:32:20 +0700

    add new welcome page

---
+++ resources/views/welcome_N219_techpub.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/bootstrap_5.2.0.min.css">
+  <script src="js/bootstrap_5.2.0.min.js"></script>
+  <title>N219 Techpub</title>
+</head>
+<body>
+  <nav class="navbar bg-body-tertiary bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand text-white">N219 Technical Publication</a>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-light text-white bg" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+
+</body>
+</html>
