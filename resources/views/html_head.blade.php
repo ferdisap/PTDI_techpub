16e6c1c790948ba10f0144de38ef27957fd54c61
Ferdi Saptoko <ferdisaptoko@gmail.com>
2023-07-28 08:42:03 +0700

    latihan xsl

--- resources/views/html_head.blade.php
+++ resources/views/html_head.blade.php
@@ -4,13 +4,20 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
   <link rel="stylesheet" href="/css/bootstrap_5.2.0.min.css">
+
   @yield('styles')
+  
   <script src="/js/bootstrap_5.3.1.bundel.min.js"></script>
+  
   @yield('scripts_onTop')
+
   <title>{{ $title ?? 'N219 Techpub' }}</title>
 </head>
 <body>
+  
   @yield('body')
+  
   @yield('scripts_onBottom')
 </body>
